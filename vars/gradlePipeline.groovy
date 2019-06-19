107b189f101e6f10308bc6b9f78b9b292dbcf9c5
Gabriel Konat <gabrielkonat@gmail.com>
2019-06-19 10:39:12 +0200

    Process JUnit result files to see test results.

--- vars/gradlePipeline.groovy
+++ vars/gradlePipeline.groovy
@@ -170,6 +170,7 @@ def call(Map args) {
           withCredentials([usernamePassword(credentialsId: publishCredentialsId, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
             sh "$gradleCommand publish -P$publishUsernameProperty=\$USERNAME -P$publishPasswordProperty=\$PASSWORD"
           }
+          junit '**/build/test-reports/*.xml'
         }
       }
     }
